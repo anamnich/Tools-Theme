faa2b5af6c5121673f3f432797aeb76a1a660217
anamnich <98353608+anamnich@users.noreply.github.com>
2023-03-04 03:07:00 +0700

    Add files via upload

---
+++ resources/script.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+SCRIPT_VERSION="V1.0.0"
+
+clear
+echo
+#print_brake 70
+echo "* Pterodactyl-AllToolsTheme Script @ $SCRIPT_VERSION"
+echo
+echo "* Copyright (C) 2021 - 2023 By Riyan Sipayung."
+echo "* https://github.com/SFams21/Tools-Theme"
+echo
+echo "* This script is not associated with the official Pterodactyl Project."
+#print_brake 70
+echo
+echo "1. Install DarkNColor"
+echo "2. Install Addons"
+echo "3. Install Animated theme"
+echo "4. Install MinecraftTheme"
+echo "5. Install Animated Login theme"
+echo "6. Restore from backup"
+echo "7. Close / Cancel"
+read -p "Please enter a number: " choice
+if [ $choice == "1" ]
+    then
+    bash <(curl -s https://raw.githubusercontent.com/SFams21/Tools-Theme/master/resources/DarkNColor.sh)
+fi
+if [ $choice == "2" ]
+    then
+    bash <(curl -s https://raw.githubusercontent.com/SFams21/Tools-Theme/master/resources/Addons.sh)
+fi
+if [ $choice == "3" ]
+    then
+    echo "Installing Particles themes..."
+    bash <(curl -sSL https://raw.githubusercontent.com/SFams21/Tools-Theme/master/resources/ParticleJS/install.sh)
+fi
+if [ $choice == "4" ]
+    then
+    echo "Installing Minecraft Theme..."
+    bash <(curl -sSL https://raw.githubusercontent.com/SFams21/Tools-Theme/master/resources/mcTheme/install.sh)
+fi
+if [ $choice == "5" ]
+    then
+    echo "Installing Animated login"
+    bash <(curl -s https://raw.githubusercontent.com/SFams21/Tools-Theme/master/resources/login/install.sh)
+fi
+if [ $choice == "6" ]
+    then
+    bash <(curl -s https://raw.githubusercontent.com/SFams21/Tools-Theme/master/resources/restore.sh)
+fi
+if [ $choice == "7" ]
+    then
+    echo "Canceling..."
+    exit 0
+fi
